7c19cb7a721b57bcfef0c3f0d134e990d5566e7f
Syed Ziaul Ameen <sameen@adobe.com>
2023-06-27 19:45:46 +0530

    upgrade react version

--- src/index.jsx
+++ src/index.jsx
@@ -1,37 +1,35 @@
-import React, { Component, Fragment } from "react";
+import React, { Fragment } from "react";
 import PropTypes from "prop-types";
 import injectSheet from "react-jss";
 import classNames from "classnames";
 import styles from "./style";
 
-class Shimmer extends Component {
-  /**
-   * Appending animating shimmer class to the Shimmer component's child.
-   */
-  appendShimmerToChild(child) {
-    const { classes } = this.props;
-    return React.cloneElement(child, {
-      className: classNames(child.props.className, classes.shimmer)
-    });
-  }
+const Shimmer = (props) => {
+	/**
+	 * Appending animating shimmer class to the Shimmer component's child.
+	 */
+	const appendShimmerToChild = (child) => {
+		const { classes } = props;
+		return React.cloneElement(child, {
+			className: classNames(child.props.className, classes.shimmer),
+		});
+	};
 
-  render() {
-    const { children } = this.props;
-    let modifiedChildren = [];
-    if (children.length > 0) {
-      children.forEach(element => {
-        modifiedChildren.push(this.appendShimmerToChild(element));
-      });
-    } else {
-      modifiedChildren = this.appendShimmerToChild(children);
-    }
-    return <Fragment>{modifiedChildren}</Fragment>;
-  }
-}
+	const { children } = props;
+	let modifiedChildren = [];
+	if (children.length > 0) {
+		children.forEach((element) => {
+			modifiedChildren.push(appendShimmerToChild(element));
+		});
+	} else {
+		modifiedChildren = appendShimmerToChild(children);
+	}
+	return <Fragment>{modifiedChildren}</Fragment>;
+};
 
 Shimmer.propTypes = {
-  classes: PropTypes.object,
-  children: PropTypes.object
+	classes: PropTypes.object,
+	children: PropTypes.object,
 };
 
 export default injectSheet(styles)(Shimmer);
